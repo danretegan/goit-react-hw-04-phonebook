87c4fd24bd33b79f3d94b1901a5897cec6f00d8a
Dan <danretegan@yahoo.com>
2024-02-02 09:02:43 +0200

    refactoring ContactForm.jsx
    Used React hooks instead of class based components.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # React homework template
-
+- React hooks
 - Emotion
--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,41 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Button';
 import styles from './ContactForm.module.css';
 
-class ContactForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      number: '',
-    };
-  }
+const ContactForm = ({ onAddContact, contacts }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleNameChange = evt => {
+  const handleNameChange = evt => {
     // Restrictionarea la text (litere, spații, apostrof și cratimă):
+
     const newTextValue = evt.target.value.replace(/[^a-zA-Z\s'-]/g, '');
-    this.setState({ name: newTextValue });
+    setName(newTextValue);
   };
 
-  handleNumberChange = e => {
+  const handleNumberChange = e => {
     // Restrictionarea la cifre, spații, cratime paranteze și + la început:
+
     const newNumberValue = e.target.value.replace(
       /[^+\d\s()-]|^[\s()-]+|(?<=\d)[+]|\b[+]\b/g,
       ''
     );
-    this.setState({ number: newNumberValue });
+    setNumber(newNumberValue);
   };
 
-  handleAddButtonClick = () => {
-    const { name, number } = this.state;
-    const { onAddContact, contacts } = this.props;
-
+  const handleAddButtonClick = () => {
     // Verificăm dacă numele sau numărul există deja în lista de contacte:
+
     const nameExists = contacts.some(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
-
     const numberExists = contacts.some(contact => contact.number === number);
 
     if (nameExists) {
@@ -44,56 +38,51 @@ class ContactForm extends Component {
       alert(`${number} is already in contacts!`);
     } else if (name.trim() !== '' && number.trim() !== '') {
       // Adăugăm contactul doar dacă nu există și câmpurile nu sunt goale:
+
       onAddContact(name, number);
-      this.setState({
-        name: '',
-        number: '',
-      });
+      setName('');
+      setNumber('');
     }
   };
 
-  render() {
-    const { name, number } = this.state;
+  return (
+    <form className={styles.container}>
+      <label className={styles.label}>
+        Name:
+        <input
+          className={styles.input}
+          type="text"
+          name="name"
+          placeholder="Name and surname:"
+          // pattern="^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={handleNameChange}
+        />
+      </label>
 
-    return (
-      <form className={styles.container}>
-        <label className={styles.label}>
-          Name:
-          <input
-            className={styles.input}
-            type="text"
-            name="name"
-            placeholder="Name and surname:"
-            // pattern="^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={name}
-            onChange={this.handleNameChange}
-          />
-        </label>
+      <label className={styles.label}>
+        Number:
+        <input
+          className={styles.input}
+          type="tel"
+          name="number"
+          placeholder="Telephone number:"
+          // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={handleNumberChange}
+        />
+      </label>
 
-        <label className={styles.label}>
-          Number:
-          <input
-            className={styles.input}
-            type="tel"
-            name="number"
-            placeholder="Telephone number:"
-            // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={number}
-            onChange={this.handleNumberChange}
-          />
-        </label>
-
-        <Button type="button" action={this.handleAddButtonClick}>
-          Add contact
-        </Button>
-      </form>
-    );
-  }
-}
+      <Button type="button" action={handleAddButtonClick}>
+        Add contact
+      </Button>
+    </form>
+  );
+};
 
 ContactForm.propTypes = {
   onAddContact: PropTypes.func.isRequired,
